b8c73f17f131e189c0679ee002dee5a3dfc7c9dc
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2024-07-07 13:04:13 +0500

    all programs

---
+++ script.js
@@ -0,0 +1,52 @@
+//1 finding the smallest number in an array:
+
+// let numbers = [];
+
+// for (let i = 0; i < 5; i++) {
+//   let userInput = prompt("Enter a number:");
+//   numbers.push(userInput);
+// }
+
+// let smallestNumber = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] < smallestNumber) {
+//     smallestNumber = numbers[i];
+//   }
+// }
+// console.log("The smallest number is: " + smallestNumber);
+
+//2 converting celcius to fahrenheit:
+
+// let celsius = 25;
+// let fahrenheit = celsius * 9 / 5 + 32;
+// console.log(fahrenheit);
+
+//3 convert fahrenheit to celcius:
+// let fahrenheit = 77;
+// let celsius = (fahrenheit - 32) * 5 / 9;
+// console.log(celsius);
+
+//4 reverse an array in js
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.reverse();
+// console.log(numbers);
+
+//5 finding an average of an array:
+// Create an empty array to hold the user inputs
+// let numbers = [];
+
+// for (let i = 0; i < 5; i++) {
+//   let userInput = prompt("Enter a number:");
+//   numbers.push(userInput);
+// }
+
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// let average = sum / numbers.length;
+
+// console.log("The average of the numbers is: " + average);
+//done
